61cb3c7dde74fbfd54a7735d99a532249b09960b
Rishi Mukherjee <thethunder666@gmail.com>
2013-10-08 08:48:29 +0530

    Added script for congratulation for job.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Some of my scripts to use in day to day life.
 
 cyber.py - I use this to log into cyberoam in college.
+congo.py - Congratulate my facebook friends for their job. :D
---
+++ congo.py
@@ -0,0 +1,39 @@
+import json
+import requests
+
+TOKEN = ""
+
+def get_posts():
+    query = ("SELECT post_id, actor_id, message FROM stream WHERE filter_key = 'others' LIMIT 200")
+    payload = {'q': query, 'access_token': TOKEN}
+    r = requests.get('https://graph.facebook.com/fql', params=payload)
+    result = json.loads(r.text)
+    return result['data']
+
+def commentall(posts):
+    for post in posts:
+        if 'placed' in post['message'].lower()  or 'hired' in post['message'].lower():
+            post_comments_payload = {'access_token': TOKEN}
+            getcomments = requests.get('https://graph.facebook.com/%s/comments' % post['post_id'], params=post_comments_payload)
+            comments = json.loads(getcomments.text)["data"]
+            already_congratulated = False
+            for comment in comments:
+                if "Congratulations! Phode Ho." in comment["message"] or "Jiyo lark" in comment["message"]: #This is hack. :P
+                    already_congratulated = True
+                    break
+            if already_congratulated:
+                continue
+            print comments
+            req = requests.get('https://graph.facebook.com/%s' % post['actor_id'])
+            comments_url = 'https://graph.facebook.com/%s/comments' % post['post_id']
+            user = json.loads(req.text)
+            message = 'Congratulations! Phode Ho. %s :D' % user['first_name']
+            payload = {'access_token': TOKEN, 'message': message}
+            #doComment = requests.post(comments_url, data=payload)
+
+            print "Congratulated %s for (%s)" % (user['first_name'], post['message'])
+            print "-----------------------------------------"
+            print user['first_name']
+            print "-----------------------------------------"
+
+commentall(get_posts())
